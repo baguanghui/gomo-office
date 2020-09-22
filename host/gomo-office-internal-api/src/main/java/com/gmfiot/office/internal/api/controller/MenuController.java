61496f5a94fb5ee06fc7d89bd5216c495bad9740
baguanghui <375949795@qq.com>
2020-09-22 13:32:31 +0800

    简介请求路径配置

--- host/gomo-office-internal-api/src/main/java/com/gmfiot/office/internal/api/controller/MenuController.java
+++ host/gomo-office-internal-api/src/main/java/com/gmfiot/office/internal/api/controller/MenuController.java
@@ -1,8 +1,11 @@
 package com.gmfiot.office.internal.api.controller;
 
 import com.gmfiot.core.data.Result;
+import com.gmfiot.office.model.User;
 import com.gmfiot.office.service.definition.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +19,9 @@ public class MenuController {
         return menuService.getList(null);
     }
 
+
     @RequestMapping(value = "menus/{id}",method = RequestMethod.GET)
-    public Result getOne(@PathVariable Long id){
+    public Result getOne(@PathVariable Long id,int a){
         System.out.println(id);
         return menuService.getById(id);
     }
--- host/gomo-office-internal-api/src/main/java/com/gmfiot/office/internal/api/controller/UsersController.java
+++ host/gomo-office-internal-api/src/main/java/com/gmfiot/office/internal/api/controller/UsersController.java
@@ -6,14 +6,12 @@ import com.gmfiot.office.model.User;
 import com.gmfiot.office.model.query.UserQuery;
 import com.gmfiot.office.service.definition.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author ThinkPad
  */
+@RequestMapping(value = "/users")
 @RestController
 public class UsersController {
 
@@ -25,17 +23,16 @@ public class UsersController {
      * @param user
      * @return
      */
-    @RequestMapping(value = "/users",method = RequestMethod.POST,produces = {"application/json"})
+    @RequestMapping(method = RequestMethod.POST,produces = {"application/json"})
     public Result saveUser(@RequestBody User user){
         System.out.println(JsonUtil.toJson(user));
         var result = userService.addUser(user);
         return  result;
     }
 
-    @RequestMapping(value = "/users/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public Result getUser(@PathVariable long id){
         var result = userService.getById(id);
-
         return result;
     }
 
@@ -44,7 +41,7 @@ public class UsersController {
      * @param id
      * @return
      */
-    @RequestMapping(value = "/users/{id}",method = RequestMethod.DELETE,produces = "application/json")
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE,produces = "application/json")
     public Result deleteUser(@PathVariable long id){
         var result = userService.deleteById(id);
         return result;
@@ -55,13 +52,13 @@ public class UsersController {
      * @param user
      * @return
      */
-    @RequestMapping(value = "/users",method = RequestMethod.PUT,produces = "application/json")
+    @RequestMapping(method = RequestMethod.PUT,produces = "application/json")
     public Result updateUser(@RequestBody User user){
         var result = userService.updateById(user);
         return result;
     }
 
-    @RequestMapping(value = "/users",method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET,produces = "application/json")
     public Result getUserList(UserQuery query){
         var result = userService.getList(query);
         return  result;
